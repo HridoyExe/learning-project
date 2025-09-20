82f95265ca1838a00a93add78d5f2e1787f99905
Md Forman Ali <formanislamhridoy@gmail.com>
2025-09-20 16:00:19 +0600

    ready for deploy

--- product/models.py
+++ product/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
 from product.validators import validator_file_size
+from cloudinary.models import CloudinaryField
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
@@ -27,8 +29,7 @@ class Product(models.Model):
     
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='images')
-    image = models.ImageField(
-        upload_to="products/images/", validators= [validator_file_size])
+    image = CloudinaryField('image')
     
     # file = models.FileField(upload_to="product/images/", validators= FileExistsError['pdf'])
 
