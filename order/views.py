35cdbb44e7804af4138b4756f0185d87d3a545ae
Md Forman Ali <formanislamhridoy@gmail.com>
2026-01-30 20:40:33 +0600

    CartViewSet Change

--- order/views.py
+++ order/views.py
@@ -9,6 +9,7 @@ from order.serializers import UpdateOrderSerializer
 from rest_framework.decorators import action
 from order.services import OrderService
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
@@ -24,6 +25,14 @@ class CartViewSet(CreateModelMixin,RetrieveModelMixin, DestroyModelMixin,Generic
         if getattr(self, 'swagger_fake_view',False):
             return Cart.objects.none()
         return Cart.objects.prefetch_related('items__product').filter(user=self.request.user) 
+    
+    def create(self, request, *args,**kwargs):
+        existing_cart = Cart.objects.filter(user=request.user).first()
+
+        if existing_cart:
+            serializer = self.get_serializer(existing_cart)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return super().create(request,*args,**kwargs)
 
 
 
